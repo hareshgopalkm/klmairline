8926901b4c69296fb01c58e3e3e7bb569671b649
hareshgopalkm <33626927+hareshgopalkm@users.noreply.github.com>
2022-12-06 18:38:22 +0530

    Delete klm-airfrance-assessment-main directory

--- klm-airfrance-assessment-main/frontend/src/app/check-in-page/check-in-page.model.ts
+++
@@ -1,6 +0,0 @@
-import { FormControl } from '@angular/forms';
-
-export interface CheckInPageForm {
-  bookingCode: FormControl<string | null>;
-  familyName: FormControl<string | null>;
-}
--- klm-airfrance-assessment-main/frontend/src/app/header/header.module.ts
+++
@@ -1,9 +0,0 @@
-import { NgModule } from '@angular/core';
-import { AppHeaderComponent } from './header.component';
-
-
-@NgModule({
-  declarations: [AppHeaderComponent],
-  exports: [AppHeaderComponent]
-})
-export class AppHeaderModule {}
